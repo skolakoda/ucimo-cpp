0feb346a390c197371c4a0a5a5a9d88a63ed854e
Damjan Pavlica <mudroljub@gmail.com>
2018-04-27 23:36:58 +0100

    mapa struct

--- 70-igre/snake-game.cpp
+++ 70-igre/snake-game.cpp
@@ -38,23 +38,28 @@ class Zmija {
 const int ZID = -1;
 const int HRANA = -2;
 const int PRAZNO = 0;
-const int TELO_ZMIJE = 1; // ne moze enum jer se povecava broj
+const int ZMIJA = 1; // ne moze enum jer se povecava broj
 
-const int VISINA_MAPE = 20;
-const int SIRINA_MAPE = 30;
-const int VELICINA_MAPE = VISINA_MAPE * SIRINA_MAPE;
-int mapa_polja[VELICINA_MAPE];
+struct Mapa {
+    int sirina = 30;
+    int visina = 20;
+    int velicina = visina * sirina;
+    int polja[30 * 20]; // ne moze velicina?
 
-Zmija zmija(VISINA_MAPE / 2, SIRINA_MAPE / 2, 3);
+    int dajPolje(int x, int y) {
+        return polja[x + y * visina];
+    }
+
+    void postaviPolje(int x, int y, int vrednost) {
+        polja[x + y * visina] = vrednost;
+    }
+};
+
+Mapa mapa;
+Zmija zmija(mapa.visina/2, mapa.sirina/2, 3);
 bool igra_ide = true;
 
-int dajPolje(int x, int y) {
-    return mapa_polja[x + y * VISINA_MAPE];
-}
-
-void setPolje(int x, int y, int vrednost) {
-    mapa_polja[x + y * VISINA_MAPE] = vrednost;
-}
+/* FUNKCIJE */
 
 char dajKarakter(int value) {
     switch (value) {
@@ -69,29 +74,29 @@ void praviHranu() {
     int x;
     int y;
     do {
-        x = rand() % (VISINA_MAPE - 2) + 1;
-        y = rand() % (SIRINA_MAPE - 2) + 1;
-    } while (dajPolje(x, y) != PRAZNO);
-    setPolje(x, y, HRANA);
+        x = rand() % (mapa.visina - 2) + 1;
+        y = rand() % (mapa.sirina - 2) + 1;
+    } while (mapa.dajPolje(x, y) != PRAZNO);
+    mapa.postaviPolje(x, y, HRANA);
 }
 
 void initMap() {
-    setPolje(zmija.x, zmija.y, TELO_ZMIJE);
-    for (int x = 0; x < VISINA_MAPE; ++x) {
-        mapa_polja[x] = ZID;
-        setPolje(x, SIRINA_MAPE - 1, ZID);
+    mapa.postaviPolje(zmija.x, zmija.y, ZMIJA);
+    for (int x = 0; x < mapa.visina; ++x) {
+        mapa.polja[x] = ZID;
+        mapa.postaviPolje(x, mapa.sirina - 1, ZID);
     }
-    for (int y = 0; y < SIRINA_MAPE; y++) {
-        setPolje(0, y, ZID);
-        setPolje(VISINA_MAPE - 1, y, ZID);
+    for (int y = 0; y < mapa.sirina; y++) {
+        mapa.postaviPolje(0, y, ZID);
+        mapa.postaviPolje(mapa.visina - 1, y, ZID);
     }
     praviHranu();
 }
 
 void crtaj() {
-    for (int x = 0; x < VISINA_MAPE; ++x) {
-        for (int y = 0; y < SIRINA_MAPE; ++y) {
-            std::cout << dajKarakter(dajPolje(x, y));
+    for (int x = 0; x < mapa.visina; ++x) {
+        for (int y = 0; y < mapa.sirina; ++y) {
+            std::cout << dajKarakter(mapa.dajPolje(x, y));
         }
         std::cout << std::endl;
     }
@@ -100,15 +105,15 @@ void crtaj() {
 void mrdajZmiju(int dx, int dy) {
     int x = zmija.x + dx;
     int y = zmija.y + dy;
-    if (dajPolje(x, y) == HRANA) {
+    if (mapa.dajPolje(x, y) == HRANA) {
         zmija.duzina++;
         praviHranu();
-    } else if (dajPolje(x, y) != PRAZNO) {
+    } else if (mapa.dajPolje(x, y) != PRAZNO) {
         igra_ide = false;
     }
     zmija.x = x;
     zmija.y = y;
-    setPolje(x, y, zmija.duzina + 1); // povecava broj glavi zmije
+    mapa.postaviPolje(x, y, zmija.duzina + 1); // povecava broj glavi zmije
 }
 
 void azurirajZmiju() {
@@ -122,8 +127,8 @@ void azurirajZmiju() {
         case Smer::LEVO: mrdajZmiju(0, -1);
             break;
     }
-    for (int i = 0; i < VELICINA_MAPE; i++) {
-        if (mapa_polja[i] >= TELO_ZMIJE) mapa_polja[i]--;   // smanjuje broj zmiji
+    for (int i = 0; i < mapa.velicina; i++) {
+        if (mapa.polja[i] >= ZMIJA) mapa.polja[i]--;   // smanjuje broj zmiji
     }
 }
 
